eeb4544251f768ec52065f469693e2a88f2af2c9
dandagrizzly <stavenessd2012@my.uwstout.edu>
2019-12-13 17:10:23 -0600

    Updates

---
+++ precache-manifest.60c45702a664561c9a0256cb875e7ef6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5590b6f24cfe56a4ee3aed87beb5962a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4253f03b6b929d48a5e9",
+    "url": "/portfolio/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "af7c9fccb6292ff03a70",
+    "url": "/portfolio/static/css/main.66a4b34d.chunk.css"
+  },
+  {
+    "revision": "4253f03b6b929d48a5e9",
+    "url": "/portfolio/static/js/2.55f62ecd.chunk.js"
+  },
+  {
+    "revision": "af7c9fccb6292ff03a70",
+    "url": "/portfolio/static/js/main.6e824969.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "44be1ccd70f33558088c75756c331dd3",
+    "url": "/portfolio/static/media/facebook.44be1ccd.png"
+  },
+  {
+    "revision": "338901b594ed6c32c569e506cfa07c21",
+    "url": "/portfolio/static/media/me.338901b5.png"
+  }
+]);
